c1cfb6f0a415e13cdad62f3b6b785c6a79e6c2b9
anya.shan <anya.shanaida@yahoo.com>
2022-10-09 23:46:03 +0300

    add function Add Contact to list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 import { Container } from 'components/App.styled';
 import { Section } from 'components/Section/Section';
 import { ContactForm } from 'components/ContactForm/ContactForm';
@@ -7,16 +8,19 @@ import { ContactList } from 'components/ContactList/ContactList';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
-  formSubmitHandler = data => {
-    console.log(data);
+  formAddContactList = data => {
+    const { name, number } = data;
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, { id: nanoid(), name, number }],
+    }));
   };
 
   deleteContact = contactId => {
@@ -30,7 +34,7 @@ export class App extends Component {
     return (
       <Container>
         <Section title="Phonebook">
-          <ContactForm onSubmit={this.formSubmitHandler} />
+          <ContactForm onSubmit={this.formAddContactList} />
         </Section>
         <Section title="Contacts">
           {/* <Filter /> */}
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -29,6 +29,7 @@ export class ContactForm extends Component {
   };
 
   render() {
+    const { name, number } = this.state;
     return (
       <Form onSubmit={this.handleFormSubmit}>
         <div>
@@ -36,7 +37,7 @@ export class ContactForm extends Component {
           <input
             type="text"
             name="name"
-            value={this.state.name}
+            value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -48,7 +49,7 @@ export class ContactForm extends Component {
           <input
             type="tel"
             name="number"
-            value={this.state.number}
+            value={number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
@@ -57,7 +58,9 @@ export class ContactForm extends Component {
             onChange={this.handleFormChange}
           />
         </div>
-        <button type="submit">Add contact</button>
+        <button type="submit" disabled={false}>
+          Add contact
+        </button>
       </Form>
     );
   }
